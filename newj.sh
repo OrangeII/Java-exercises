810f58f593ad451c3fcd7c313cb6c108796f5e54
Nicolò Ivaldi <ivaldinico@gmail.com>
2016-11-04 16:52:43 +0100

    correct error in the template

--- newj.sh
+++ newj.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #this creates a new java class with main method named as the parameter
-echo -e "class $1(){\n\tpublic static void main(String[] args){\n\t\t\n\t}\n}" > $1.java
+echo -e "class $1{\n\tpublic static void main(String[] args){\n\t\t\n\t}\n}" > $1.java
 #this opens the file in Atom
 atom $1.java
