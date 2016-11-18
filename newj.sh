754dd64f84d003911b9dd86d80405e701d5db7a5
Nicolò Ivaldi <ivaldinico@gmail.com>
2016-11-18 13:30:57 +0100

    add symlink to ~/bin/

--- newj.sh
+++ newj.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#this script is symlinked to my  ~/bin/ folder
 #this creates a new java class with main method named as the parameter
 echo -e "class $1{\n\tpublic static void main(String[] args){\n\t\t\n\t}\n}" > $1.java
 #this opens the file in Atom
