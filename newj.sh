dada5f450f592cd0f5c3d21123ba18895fe33451
Nicolò Ivaldi <ivaldinico@gmail.com>
2017-05-17 11:57:46 +0200

    add future plans for newj.sh script

--- newj.sh
+++ newj.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 #this script is symlinked to my  ~/bin/ folder
+#parameters:
+# $1 class name
+
+#WANNADO:
+#options:
+# -m generate main method
+# -e <superclass> generate extends statement
+# -i <interface> generate implements
+
 #this creates a new java class with main method named as the parameter
 echo -e "class $1{\n\tpublic static void main(String[] args){\n\t\t\n\t}\n}" > $1.java
 #this opens the file in Atom
